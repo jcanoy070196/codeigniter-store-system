6dfc70ded47b2f878110195b559944b925479fb5
Julius Mamert Canoy <jcanoy070196@yahoo.com>
2018-07-16 07:25:52 +0000

    Update login.php

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,1 +1,25 @@
-H@“ ã]Š÷%jË
+<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="col-lg-4"></div>
+				<div class="col-lg-4">
+    				<form>
+                      <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="username" class="form-control" id="username"  placeholder="Enter username">
+                      </div>
+                      <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                      </div>
+                      <button type="submit" class="btn btn-danger">Login</button>
+                    </form>	
+				</div>
+                <div class="col-lg-4">
+                    
+                </div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
