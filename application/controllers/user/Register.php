eb3f64eebdd297456196ccbe3842981ef9246b01
Julius Mamert Canoy <jcanoy070196@yahoo.com>
2018-07-16 08:01:51 +0000

    Update Register.php

---
+++ application/controllers/user/Register.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Register extends CI_Controller {
+    
+    function index(){
+        $data['account_name'] = 'Test2';
+        $data['account_balance'] = '2500';
+        
+        $this->load->view('common/header');
+        $this->load->view('common/nav',$data);
+        $this->load->view('user/register');
+        $this->load->view('common/footer');
+    }
+    
+    
+}
