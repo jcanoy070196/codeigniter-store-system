c99169184a6fb45763868464c45c0d4db0c683d7
Julius Mamert Canoy <jcanoy070196@yahoo.com>
2018-07-16 07:26:51 +0000

    Update register.php

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -1,1 +1,33 @@
-H@“ ã]Š÷%jË
+<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="col-lg-4"></div>
+				<div class="col-lg-4">
+    				<form>
+                      <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="username" class="form-control" id="username"  placeholder="Enter username">
+                      </div>
+                      <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                      </div>
+                      <div class="form-group">
+                        <label for="confirmPassword">Password</label>
+                        <input type="confirmPassword" class="form-control" id="confirmPassword" placeholder="Confirm Password">
+                      </div>
+                      <div class="form-group">
+                        <label for="email">Username</label>
+                        <input type="email" class="form-control" id="email"  placeholder="Enter email">
+                      </div>
+                      <button type="submit" class="btn btn-danger">Login</button>
+                    </form>	
+				</div>
+                <div class="col-lg-4">
+                    
+                </div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
