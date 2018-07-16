3130d6fa7e1924cbd68775612aa2578392f495e4
Julius Mamert Canoy <jcanoy070196@yahoo.com>
2018-07-16 08:07:53 +0000

    Update register.php

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -15,11 +15,30 @@
                       </div>
                       <div class="form-group">
                         <label for="confirmPassword">Password</label>
-                        <input type="confirmPassword" class="form-control" id="confirmPassword" placeholder="Confirm Password">
+                        <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password">
                       </div>
                       <div class="form-group">
-                        <label for="email">Username</label>
+                        <label for="email">Email</label>
                         <input type="email" class="form-control" id="email"  placeholder="Enter email">
+                      </div>
+                      <div class="form-group">
+                        <label for="firstname">Username</label>
+                        <input type="text" class="form-control" id="firstname"  placeholder="Enter Firstname">
+                      </div>
+                      <div class="form-group">
+                        <label for="middlename">Username</label>
+                        <input type="text" class="form-control" id="middlename"  placeholder="Enter Middlename">
+                      </div>
+                      <div class="form-group">
+                        <label for="lastname">Username</label>
+                        <input type="text" class="form-control" id="lastname"  placeholder="Enter Lastname">
+                      </div>
+                      <div class="form-group">
+                        <label for="gender">Gender</label>
+                        <select>
+                            <option>Male</option>
+                            <option>Female</option>
+                        </select>
                       </div>
                       <button type="submit" class="btn btn-danger">Login</button>
                     </form>	
