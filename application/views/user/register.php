4e53d51f87cc1ac3cc1186803a93bcde1f6ab387
Julius Mamert Canoy <jcanoy070196@yahoo.com>
2018-07-16 08:32:15 +0000

    Update register.php

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -22,19 +22,20 @@
                         <input type="email" class="form-control" id="email"  placeholder="Enter email">
                       </div>
                       <div class="form-group">
-                        <label for="firstname">Username</label>
+                        <label for="firstname">Firstname</label>
                         <input type="text" class="form-control" id="firstname"  placeholder="Enter Firstname">
                       </div>
                       <div class="form-group">
-                        <label for="middlename">Username</label>
+                        <label for="middlename">Middlename</label>
                         <input type="text" class="form-control" id="middlename"  placeholder="Enter Middlename">
                       </div>
                       <div class="form-group">
-                        <label for="lastname">Username</label>
+                        <label for="lastname">Lastname</label>
                         <input type="text" class="form-control" id="lastname"  placeholder="Enter Lastname">
                       </div>
                       <div class="form-group">
                         <label for="gender">Gender</label>
+                        <br>
                         <select id="gender">
                             <option>Male</option>
                             <option>Female</option>
